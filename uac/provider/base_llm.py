76e83016e80d9ae49ecc3be9f94a6a4491546f11
Longtao Zheng <44831678+ltzheng@users.noreply.github.com>
2023-11-07 13:54:01 +0800

    Add GPT-4V provider and util functions (#11)

--- uac/provider/base_llm.py
+++ uac/provider/base_llm.py
@@ -1,6 +1,13 @@
 """Base class for model providers."""
 import abc
 
+from typing import (
+    List,
+    Dict,
+    Tuple,
+    Optional,
+)
+
 from uac.config import Config
 from uac.utils import AbstractSingleton
 
@@ -9,7 +16,13 @@ cfg = Config()
 
 class LLMProviderSingleton(AbstractSingleton):
     @abc.abstractmethod
-    def create_completion(self, data):
+    def create_completion(
+        self,
+        messages: List[Dict[str, str]],
+        model: str,
+        temperature: float,
+        stop_tokens: Optional[List[str]] = None,
+    ) -> Tuple[str, Dict[str, int]]:
         pass
 
     @abc.abstractmethod
