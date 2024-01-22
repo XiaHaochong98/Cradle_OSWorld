XiaHaochong98/Cradle_OSWorld

commit a10e59266c9dc7fb94a7005da798b4e01714e723

Refinements to skill library and timing/pause (#210)

uac/gameio/skill_registry.py
@@ -70,9 +70,9 @@ class SkillRegistry:
         self.from_local = from_local
         if skill_scope == 'Basic':
             self.skill_library_filename = BASIC_SKILL_LIB_FILE
-        if skill_scope == 'Full':
+        elif skill_scope == 'Full':
             self.skill_library_filename = EXPL_SKILL_LIB_FILE
-        if skill_scope == None:
+        elif skill_scope == None:
             self.from_local = False
 
         self.skill_scope = skill_scope
@@ -137,6 +137,10 @@ class SkillRegistry:
             raise ValueError(f"Function '{name}' not found in the registry.")
 
 
+    def execute_nop_skill():
+        time.sleep(2)
+
+
     def convert_expression_to_skill(self, expression: str = "open_map()"):
         skill_name, skill_params = self.extract_function_info(expression)
         return skill_name, skill_params
@@ -198,15 +202,19 @@ class SkillRegistry:
         def lower_func_name(skill_code):
             skill_name = get_func_name(skill_code)
             replaced_name = skill_name.lower()
-            # To make sure the skills in .py files will not be overwritten. 
+
+            # To make sure the skills in .py files will not be overwritten.
             # The skills not in .py files can still be overwritten.
             # Don't worry, this operator rarely happends.
             if replaced_name in SKILL_REGISTRY:
                 replaced_name = replaced_name+'_generated'
+
             return skill_code.replace(skill_name, replaced_name)
+
 
         def get_func_name(skill_code):
             return skill_code.split('def ')[-1].split('(')[0]
+
 
         def check_param_description(skill) -> bool:
             docstring = inspect.getdoc(skill)
@@ -222,15 +230,17 @@ class SkillRegistry:
             else:
                 return True
 
+
         def check_protection_conflict(skill):
             for word in ALLOW_LIST_TERMS:
                 if word in skill:
                     return True
+
             for word in DENY_LIST_TERMS:
                 if word in skill:
                     return False
-            return True
 
+            return True
 
         info = None
 
@@ -247,7 +257,7 @@ class SkillRegistry:
             if skill_name in self.skill_registry:
                 self.delete_skill(skill_name)
                 logger.write(f"Skill '{skill_name}' will be overwritten.")
-                
+
 
         if skill_name in self.skill_registry:
             info = f"Skill '{skill_name}' already exists."

general_template_example.py
@@ -10,7 +10,7 @@ from uac.planner.planner import Planner
 from uac.memory import LocalMemory
 from uac.provider.openai import OpenAIProvider
 from uac.provider import GdProvider
-from uac.gameio.lifecycle.ui_control import switch_to_game, pause_game, unpause_game
+from uac.gameio.lifecycle.ui_control import switch_to_game
 from uac.gameio.video.VideoRecorder import VideoRecorder
 from uac.gameio.video.VideoFrameExtractor import VideoFrameExtractor
 from uac.gameio.atomic_skills.trade_utils import __all__ as trade_skills
@@ -27,7 +27,6 @@ from groundingdino.util.inference import load_image
 
 config = Config()
 logger = Logger()
-
 
 
 def main_test_decision_making(planner_params, task_description, skill_library):
@@ -341,6 +340,7 @@ def skill_library_test():
 
     gm.store_skills()
 
+
 def main_test_gather_information(image_path = ""):
 
     llm_provider_config_path = './conf/openai_config.json'
@@ -450,7 +450,7 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
 
     time.sleep(2)
     end_frame_id = videocapture.get_current_frame_id()
-    pause_game()
+    gm.pause_game()
 
     while not success:
 
@@ -473,16 +473,18 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
             #configure the gather_information module
             gather_information_configurations = {
                 "frame_extractor": True, # extract text from the video clip
-                "marker_matcher": True,
+                "marker_matcher": False,
                 "llm_description": True, # get the description of the current screenshot
                 "object_detector": True
             }
             input["gather_information_configurations"] = gather_information_configurations
+
             # modify the general input for gather_information here
             image_introduction=[get_text_image_introduction[-1]]
             input["task_description"] = task_description
             input["video_clip_path"] = video_clip_path
             input["image_introduction"] = image_introduction
+
             # Modify the input for get_text module in gather_information here
             text_input["image_introduction"] = get_text_image_introduction
             input["text_input"] = text_input
@@ -593,6 +595,7 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
             #@TODO Temporary solution with fake augmented entries if no bounding box exists. Ideally it should read images, then check for possible augmentation.
             image_memory = memory.get_recent_history("image", k=config.decision_making_image_num)
             augmented_image_memory = memory.get_recent_history(constants.AUGMENTED_IMAGES_MEM_BUCKET, k=config.decision_making_image_num)
+
             image_introduction = []
             for i in range(len(image_memory), 0, -1):
                 if augmented_image_memory[-i] != constants.NO_IMAGE:
@@ -639,7 +642,7 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
             skill_steps = skill_steps[:number_of_execute_skills]
             logger.write(f'Skill Steps: {skill_steps}')
 
-            unpause_game()
+            gm.unpause_game()
             # TODO: find a better name of the GENERAL_GAME_INTERFACE
             if pre_screen_classification == constants.GENERAL_GAME_INTERFACE and screen_classification != constants.GENERAL_GAME_INTERFACE and pre_action:
                 exec_info = gm.execute_actions([pre_action])
@@ -651,7 +654,7 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
             cur_screen_shot_path, _ = gm.capture_screen()
 
             end_frame_id = videocapture.get_current_frame_id()
-            pause_game()
+            gm.pause_game()
 
             pre_action = exec_info["last_skill"] # exec_info also has the list of successfully executed skills. skill_steps is the full list, which may differ if there were execution errors.
 
@@ -707,6 +710,7 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
                     }
                 ]
                 input["image_introduction"] = image_introduction
+
                 input["task_description"] = task_description
                 input["previous_action"] = memory.get_recent_history("action", k=1)[-1]
                 input["previous_reasoning"] = memory.get_recent_history("decision_making_reasoning", k=1)[-1]
@@ -718,7 +722,9 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
                 success = data['res_dict']['success']
                 success_reasoning = data['res_dict']['reasoning']
                 success_criteria = data['res_dict']['criteria']
+
                 memory.add_recent_history("success_detection_reasoning", success_reasoning)
+
                 logger.write(f'Success: {success}')
                 logger.write(f'Success criteria: {success_criteria}')
                 logger.write(f'Success reason: {success_reasoning}')
@@ -747,11 +753,13 @@ def main_pipeline(planner_params, task_description, skill_library, use_success_d
                 input["task_description"] = task_description
                 input['skill_library'] = skill_library
                 input["previous_reasoning"] = pre_decision_making_reasoning
+
                 if pre_action:
                     input["previous_action"] = pre_action
                     pre_action_name, pre_action_params = gm.skill_registry.convert_expression_to_skill(pre_action)
                     action_code, action_code_info = gm.get_skill_library_in_code(pre_action_name)
                     input['action_code'] = action_code if action_code is not None else action_code_info
+
                 if exec_info["errors"]:
                     input['executing_action_error']  = exec_info["errors_info"]
 
@@ -811,7 +819,7 @@ if __name__ == '__main__':
         }
     }
 
-    skill_library = ['turn', 'move_forward', 'turn_and_move_forward', 'follow', 'shoot', 'shoot_people', 'shoot_wolves', 'choose_weapons_at', 'aim_and_shoot']
+    skill_library = ['turn', 'move_forward', 'turn_and_move_forward', 'follow', 'shoot_people', 'shoot_wolves', 'choose_weapons_at']
     #skill_library = move_skills + follow_skills
     #task_description =  "Follow Dutch."
     #task_description =  "Hitch your horse."

res/prompts/templates/gather_text_information.prompt
@@ -15,57 +15,57 @@ Task_guidance: The game will give guidance on what should be done to proceed wit
 Action_guidance: The game will give guidance on how to perform a task with keys in the keyboard or mouse, you must write a code based on the on-screen text. The content of the code should obey the following rules:
 The code rules are as follows:
 1. You should refer to different examples strictly based on the word used to control the key, such as 'use', 'hold', 'release', and 'press'.
-2. If 'use' or 'press' is in the prompt to control the key, io_env.key_press must be used to act on it and specify a press duration argument (in seconds). Refer to Examples 1, 2, and 3.
+2. If 'use' or 'press' is in the prompt to control the key, io_env.key_press must be used to act on it. If it should be a short press, do not specify any duration argument. If it's a long press, a press duration argument (in seconds). Refer to Examples 1, 2, and 3.
 3. If 'hold' is in the prompt to control the key, it means keeping the key pressed (maybe indefinitely). Each io_env.key_hold must specify a press duration argument for the hold (if not indefinitely). Refer to Example 4.
 4. All durations are set to a minimum of 2 seconds by default. You can choose a longer or shorter duration. If it should be indefinite, do not specify a duration argument.
 5. The name of the created function should only use phrasal verbs, verbs, nouns, or adverbs shown in the prompt and should be in the verb+noun or verb+adverb format, such as drink_water, slow_down_car, ride_faster. Note that words that do not show in the prompt are prohibited.
 
-This is Example 1. If "use" is in the prompt and the text prompt on the screenshot is "use E to drink water", your output should be:
-```python
-def drink_water():
-    """
-    use "e" to drink water
-    """
-    io_env.key_press('e', 2)
-```
-This is Example 2. If "press" is in the prompt and the text prompt on the screenshot is "press E to play the card", your output should be:
+This is Example 1. If "press" is in the prompt and the text prompt on the screenshot is "press X to play the card", your output should be:
 ```python
 def play_card():
     """
-    press "e" to play the card
+    press "x" to play the card
     """
-    io_env.key_press('e')
+    io_env.key_press('x')
 ```
-This is Example 3. If "use" is in the prompt and the text prompt on the screenshot is "use E and Q to jump the barrier", your output should be:
+This is Example 2. If "use" is in the prompt and the text prompt on the screenshot is "use ENTER to drink water", your output should be:
+```python
+def drink_water():
+    """
+    use "enter" to drink water
+    """
+    io_env.key_press('enter', 2)
+```
+This is Example 3. If "use" is in the prompt and the text prompt on the screenshot is "use W and J to jump the barrier", your output should be:
 ```python
 def jump_barrier():
     """
-    use "e" and "q" to jump the barrier
+    use "w" and "j" to jump the barrier
     """
-    io_env.key_press('e,q', 3)
+    io_env.key_press('w,j', 3)
 ```
-This is Example 4. If "hold" is in the prompt and the text prompt on the screenshot is "hold E to run", your output should be:
+This is Example 4. If "hold" is in the prompt and the text prompt on the screenshot is "hold H to run", your output should be:
 ```python
 def run():
     """
-    hold "e" to run
+    hold "h" to run
     """
-    io_env.key_hold('e')
+    io_env.key_hold('h')
 ```
-This is Example 5. If "release" is in the prompt and the text prompt on the screenshot is "release E to drop the items", your output should be:
+This is Example 5. If "release" is in the prompt and the text prompt on the screenshot is "release Q to drop the items", your output should be:
 ```python
 def drop_items():
     """
-    release "e" to drop the items
+    release "q" to drop the items
     """
-    io_env.key_release('e')
+    io_env.key_release('q')
 ```
 
 Dialogue: Conversations between game characters. The content of this should be in the format of "character name: dialogue". For example "Arthur: I'm fine".
 
 Other: Other information that does not belong to the above categories.
 
-Reasoning: The reasons for classification for each piece of information. 
+Reasoning: The reasons for classification for each piece of information.
 For Action Guidance, based on the word used to prompt to control the key, such as press, hold, release, what code rules should it follow?
 
 The information should be in the following categories, you should output the following content without adding any other explanation or information. If there is no content for some categories, do not output anything for these categories:

uac/config/config.py
@@ -78,7 +78,7 @@ class Config(metaclass=Singleton):
 
         # Parallel request to LLM parameters
         self.parallel_request_gather_information = True
-        
+
         #Skill retrieval
         self.skill_from_local = True
         self.skill_local_path = './res/skills/'

uac/gameio/atomic_skills/hunt.py
@@ -6,7 +6,7 @@ config = Config()
 io_env = IOEnvironment()
 
 
-@register_skill("aim_and_shoot")
+#@register_skill("aim_and_shoot")
 def aim_and_shoot(x, y):
     """
     Aim and shoots the weapon in the game.
@@ -19,7 +19,7 @@ def aim_and_shoot(x, y):
     io_env.mouse_click_button(button=io_env.LEFT_MOUSE_BUTTON, clicks=2)
 
 
-@register_skill("aim")
+#@register_skill("aim")
 def aim(x, y):
     """
     Aim the weapon in the game.
@@ -42,7 +42,7 @@ def choose_weapons_at(x, y):
     io_env.mouse_move_normalized(x, y)
 
 
-@register_skill("shoot")
+#@register_skill("shoot")
 def shoot(x, y):
     """
     Shoot the weapon at a specific location in view.
@@ -74,8 +74,8 @@ def view_weapons():
 
 
 __all__ = [
-    "aim_and_shoot",
-    "shoot",
+    #"aim_and_shoot",
+    #"shoot",
     "choose_weapons_at",
     "view_weapons",
     #"call_animals",

uac/gameio/game_manager.py
@@ -44,12 +44,6 @@ class GameManager:
         exit_back_to_pause()
 
 
-    def list_bootstrap_skills(self):
-
-        all_skills = self.skill_registry.get_all_skills()
-        return self.get_filtered_skills(all_skills)
-
-
     def get_skill_information(self, skill_list):
 
         filtered_skill_library = []
@@ -88,7 +82,7 @@ class GameManager:
 
         if action == "navigate_path":
 
-            time.sleep(3)
+            time.sleep(2)
             navigate_path(total_time_step)
 
 
@@ -104,7 +98,9 @@ class GameManager:
         io_env.update_timeouts()
 
         if actions is None or len(actions) == 0 or actions == '' or actions[0] == '':
-            logger.warn(f"No actions to execute!")
+            logger.warn(f"No actions to execute! Executing nop.")
+            self.skill_registry.execute_nop_skill()
+
             exec_info["errors"] = True
             exec_info["errors_info"] = "No actions to execute!"
             return exec_info

uac/gameio/io_env.py
@@ -75,7 +75,6 @@ class IOEnvironment(metaclass=Singleton):
     KEY_KEY = 'key'
     EXPIRATION_KEY = 'expiration'
 
-    # Key actions
     # All key interactions are now tracked and use the same calling structure
     # - Release is equivalent to keyUp. I.e., release a key that was pressed or held.
     # - Hold is equivalent to keyDown. I.e., hold a key for a certain duration, probably while something else happens.
@@ -85,7 +84,14 @@ class IOEnvironment(metaclass=Singleton):
     ACTION_RELEASE = 'release'
 
     # List of keys currently held. To be either released by specific calls or after timeout (max iterations).
+    # {
+    #     self.KEY_KEY: key,
+    #     self.EXPIRATION_KEY: self.MAX_ITERATIONS
+    # }
     held_keys = []
+
+    # Used currently due to an issue with pause in RDR2
+    backup_held_keys = []
 
 
     def __init__(self) -> None:
@@ -171,6 +177,23 @@ class IOEnvironment(metaclass=Singleton):
 
         self.held_keys = tmp_list.copy()
         del tmp_list
+
+
+    def handle_hold_in_pause(self):
+        self.backup_held_keys = self.held_keys.copy()
+        if self.backup_held_keys is not None and self.backup_held_keys != []:
+            for e in self.backup_held_keys:
+                pydirectinput.keyUp(e[self.KEY_KEY])
+
+        self.held_keys = []
+
+
+    def handle_hold_in_unpause(self):
+        if self.backup_held_keys is not None and self.backup_held_keys != []:
+            for e in self.backup_held_keys:
+                pydirectinput.keyDown(e[self.KEY_KEY])
+
+            self.held_keys = self.backup_held_keys.copy()
 
 
     def list_session_screenshots(self, session_dir: str = config.work_dir):

uac/gameio/lifecycle/ui_control.py
@@ -17,10 +17,12 @@ from uac.log import Logger
 from uac.gameio import IOEnvironment
 from uac.utils.template_matching import match_template_image
 
-
 config = Config()
 logger = Logger()
 io_env = IOEnvironment()
+
+PAUSE_SCREEN_WAIT = 1
+
 
 def pause_game():
 
@@ -36,7 +38,9 @@ def pause_game():
     # While game is paused, quickly re-center mouse location on x axis to avoid clipping at game window border with time
     io_env.ahk.mouse_move(config.game_resolution[0] // 2, config.game_resolution[1] // 2, speed=1, relative=False)
 
-    time.sleep(1)
+    io_env.handle_hold_in_pause()
+
+    time.sleep(PAUSE_SCREEN_WAIT)
 
 
 def unpause_game():
@@ -44,7 +48,9 @@ def unpause_game():
         pydirectinput.press('esc')
     else:
         logger.debug("The environment is not paused!")
-    time.sleep(1)
+    time.sleep(PAUSE_SCREEN_WAIT)
+
+    io_env.handle_hold_in_unpause()
 
 
 def exit_back_to_pause():
@@ -55,7 +61,7 @@ def exit_back_to_pause():
     while not is_env_paused() and back_steps < max_steps:
         back_steps += 1
         pydirectinput.press('esc')
-        time.sleep(1)
+        time.sleep(PAUSE_SCREEN_WAIT)
 
     if back_steps >= max_steps:
         logger.warn("The environment fails to pause!")

commit 047f5dd8ed126998a6e07df2d6c6b25b90f47075

Fixing bug caused by missed push in #210 (#213)

uac/gameio/skill_registry.py
@@ -137,7 +137,7 @@ class SkillRegistry:
             raise ValueError(f"Function '{name}' not found in the registry.")
 
 
-    def execute_nop_skill():
+    def execute_nop_skill(self):
         time.sleep(2)
 
 