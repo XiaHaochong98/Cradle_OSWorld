6de795242f7d93ce5b776c9eb649488a71f08bfd
XiaHaochong98 <HAOCHONG001@e.ntu.edu.sg>
2024-05-22 18:06:40 +0800

    update skills

--- app_runner.py
+++ app_runner.py
@@ -860,7 +860,8 @@ class PipelineRunner():
             # assamble the skill to a script for osworld
             import_source="import pyautogui"
             logger.write(f"Skill: {skill}")
-            skill_source_code = self.gm.get_skill_source_code(skill)
+            skill_name = skill.split('def ')[-1].split('(')[0]
+            skill_source_code = self.gm.get_skill_source_code(skill_name)
             logger.write(f"Skill source code: {skill_source_code}")
             skill_execution= skill
             skill_script = f"{import_source}\n{skill_source_code}\n{skill_execution}"
